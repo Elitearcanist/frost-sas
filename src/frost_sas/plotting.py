847d6b2dbc310cb190c977856e6b115759b2132e
EliteArcanist <andrew@jmcdfam.net>
2026-09-25 15:25:17 -0600

    make the plots look better

--- src/frost_sas/plotting.py
+++ src/frost_sas/plotting.py
@@ -2,7 +2,7 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
-from read_stave import read_stave_data_hdf5
+from data_importing.read_stave import read_stave_data_hdf5
 
 
 # From https://moonbooks.org/Articles/How-to-change-imshow-aspect-ratio-in-matplotlib-/
@@ -37,24 +37,33 @@ def plotHdf5():
     #
     # Plot magnitude
     #
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(12, 6))
 
-    ax.imshow(abs(data.data))  # , extent=[-1,1,-10,10])
-    plt.title("Magnitude")
-    forceAspect(ax, aspect=1.0)
+    pos1 = ax1.imshow(
+        abs(data.data), cmap="BuPu", aspect="auto"
+    )  # , extent=[-1,1,-10,10])
+    ax1.set_title("Magnitude")
+    ax1.set_xlabel("Pulses * N_Receivers")
+    ax1.set_ylabel("Sample")
+    fig.colorbar(pos1, ax=ax1)
+    # forceAspect(ax1, aspect=1.0)
     # fig.savefig("magnitude.png", bbox_inches='tight')
-    plt.show()
+    # plt.show()
 
     #
     # Plot phase
     #
     if data.data.dtype == np.dtype("complex128"):
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-
-        ax.imshow(np.angle(data.data))  # , extent=[-1,1,-10,10])
-        plt.title("Phase")
-        forceAspect(ax, aspect=1.0)
+        pos2 = ax2.imshow(
+            np.angle(data.data), cmap="BuPu", aspect="auto"
+        )  # , extent=[-1,1,-10,10])
+        ax2.set_title("Phase")
+        ax2.set_xlabel("Pulses * N_Receivers")
+        fig.colorbar(pos2, ax=ax2)  # TODO data is in radians
+        # forceAspect(ax2, aspect=1.0)
         # fig.savefig("phase.png", bbox_inches='tight')
-        plt.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plotHdf5()
